8cc384384b2018fcf7fc6de231e78f7aa4cb664b
Jane <onoprienkoyana@gmail.com>
2017-04-20 23:44:33 +0300

    Выводим количество ссылок по каждой странице, по которой мы прошли; Возвращаем список битых ссылок

--- parser_checker.py
+++ parser_checker.py
@@ -3,21 +3,28 @@ from bs4 import BeautifulSoup
 
 
 def get_links(html_text, protocol):  # Функция вытаскивает ссылки
+    broken_links = []
+    visited_links = {}  #счетчик посещенных страниц
     soup = BeautifulSoup(html_text, "html.parser")
     for link in soup.find_all("a"):
         href = link.get("href")
         if href.startswith('//'):  # если ссылка начинается с //
             href = "{}:{}".format(protocol, href)  # добавим протокол к этой ссылки,чтобы ссылка стала верной
-        check_link(href)
+        visited_links[href] = visited_links.get(href, 0) + 1  #если ссылки не было,значение будет единица,если была то прибавляем +1
+        link_is_broken = check_broken_link(href)  # Вызов функции проверки,битая ли ссылка
+        if link_is_broken == True:  # Если ссылка битая
+            broken_links.append(href)  # Добавляем в список битых ссылок
+    return broken_links, visited_links
 
 
-def check_link(link):  # Проверяет ссылки
+def check_broken_link(link):  # Проверяет битая ли ссылка
     try:
         req = requests.get(link)
         if req.status_code != 200:  # отсутствие успешного ответа
-            print(link)
+            return True
     except requests.exceptions.RequestException:  # ловим исключения ,которые бросает requests
-        print(link)
+        return True
+    return False
 
 
 if __name__ == '__main__':
@@ -25,4 +32,4 @@ if __name__ == '__main__':
     print(site_name)
     r = requests.get(site_name)  # получаем обратно обьект типа requests
     protocol = site_name.split("://")[0]  # получаем протокол из введенной пользователем ссылки
-    get_links(r.text, protocol)
+    broken_links, visited_links = get_links(r.text, protocol)
