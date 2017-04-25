e728ebeb2ec84658b13da08295ebcf730bdc840a
Jane <onoprienkoyana@gmail.com>
2017-04-25 22:30:50 +0300

    Добавлена статистика по ссылкам

--- parser_checker.py
+++ parser_checker.py
@@ -2,19 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_links(html_text, protocol):  # Функция вытаскивает ссылки
+def get_links(html_text, protocol , site_name):  # Функция вытаскивает ссылки
     broken_links = []
     visited_links = {}  #счетчик посещенных страниц
     soup = BeautifulSoup(html_text, "html.parser")
+    external_links_cnt = 0  # Счетчик внешних ссылок
+    internal_links_cnt = 0  # Счетчик внутренних ссылок
     for link in soup.find_all("a"):
         href = link.get("href")
         if href.startswith('//'):  # если ссылка начинается с //
             href = "{}:{}".format(protocol, href)  # добавим протокол к этой ссылки,чтобы ссылка стала верной
+        if site_name in href:
+            internal_links_cnt += 1  #Проверка внешних ссылок
+        else:
+            external_links_cnt += 1  #Проверка внутренних ссылок
         visited_links[href] = visited_links.get(href, 0) + 1  #если ссылки не было,значение будет единица,если была то прибавляем +1
         link_is_broken = check_broken_link(href)  # Вызов функции проверки,битая ли ссылка
-        if link_is_broken == True:  # Если ссылка битая
+        if link_is_broken:  # Если ссылка битая
             broken_links.append(href)  # Добавляем в список битых ссылок
-    return broken_links, visited_links
+    return broken_links, visited_links, internal_links_cnt, external_links_cnt
 
 
 def check_broken_link(link):  # Проверяет битая ли ссылка
@@ -32,4 +38,5 @@ if __name__ == '__main__':
     print(site_name)
     r = requests.get(site_name)  # получаем обратно обьект типа requests
     protocol = site_name.split("://")[0]  # получаем протокол из введенной пользователем ссылки
-    broken_links, visited_links = get_links(r.text, protocol)
+    broken_links, visited_links, internal_links_cnt, external_links_cnt = get_links(r.text, protocol, site_name)
+    print("Кол-во внешних ссылок: {}, кол-во внутренних ссылок: {}, общее коли-во ссылок: {}".format(external_links_cnt, internal_links_cnt, external_links_cnt+internal_links_cnt))
