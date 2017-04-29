3419815958f99d3bc6bb6843fece15cb190821ae
Artem Vakulenko <vklnka@ms45.edu.ru>
2017-04-29 17:12:57 +0300

    Благополучно обходит в цикле сайт

--- crawler.py
+++ crawler.py
@@ -1,46 +1,75 @@
+import sys
+from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
-def take_name():
+# проверяем задали ли название сайта из консоли
+def сhecking_the_string_parameters():
+    if len(sys.argv) < 2:
+        site = input_name_from_console()
+        return site
+    elif len(sys.argv) >= 2:
+        site = sys.argv[1]
+        return site
+
+
+# вводим адрес сайта вручную если не задали  из консоли
+def input_name_from_console():
     site_name = input("Введите адрес сайта ")
-    print(site_name)
+    print('Введена вот такая ссылка: ', site_name)
     return site_name
 
 
-def crawler(aaa):
-    page_name = aaa
-    visited_links.append(page_name) # добавляем к списку посещенных страниц что бы не ходить дважды на одну страницу
-    r = requests.get(page_name)  # получаем обратно обьект типа requests
+# парсим страничку
+def crawl_pages_at_url(page_url):
+    crawl_pages = []
+    visited_links.append(page_url)  # добавляем к списку посещенных страниц что бы не ходить дважды на одну страницу
+    if site not in page_url:  # если ссылка внешняя
+        external_links.append(page_url)  # добавляем к списку внешних ссылок
+        return False
+    r = requests.get(page_url)  # получаем обратно обьект типа requests
     soup = BeautifulSoup(r.text, 'html.parser')  # скармливаем обьект типа реквест и указываем .text что бы получить из него только текст, и указываем в качестве синтаксического анализатора (парсера) html.parser
     for link in soup.find_all('a'):  # магия указанная в типичном способе выдирания ссылок в бьютифол супе
-        link_name = str(link.get('href'))
-        if site not in link_name: # если ссылка внешняя
-            external_links.append(link.get('href'))  #добавляем к списку внешних ссылок
-        elif site in link_name: # если ссылка внутреняя
-            if link_name not in page_list: #проверяем что мы её ещё не добавляли
-                page_list.append(link.get('href'))  # добавляем к списку всех ссылок
+        link_on_page = check_suitability_of_link(link, page_url)
+        if link_on_page:
+            crawl_pages.append(link_on_page)
+    page_list.extend(crawl_pages)
+
+
+# проверяем на относительные ссылки и прочие
+def check_suitability_of_link(link, page_url):
+    link_name = str(link.get('href'))
+    if link_name == "":
+        return False
+    elif link_name[0] == "#": #избавляемся от меню и прочего начниаюшегося с #
+        return False
+    absolute_link = urljoin(page_url, link_name)
+    link_pars = urlparse(absolute_link)
+    if link_pars.scheme == "tel" or link_pars.scheme == "mailto": #отсеиваем телефоны и емейлы
+        return False
+    else:
+        return absolute_link
+
 
 if __name__ == '__main__':
     visited_links = []
     external_links = []
     page_list = []
-    site = take_name()
-    a = crawler(site)
+
+    if len(sys.argv) < 2:
+        site = input_name_from_console()
+    elif len(sys.argv) >= 2:
+        print(sys.argv[1])
+
+    crawl_pages_at_url(site)
     for page in page_list:
         if page in visited_links:
             pass
+        elif site not in page:
+            pass
         elif page not in visited_links:
-            print(page)
-            crawler(page)
-
-    print(external_links)
-
-
-
-
-
-#Сделай скрипт, который будет просить ввести имя сайта и ходить по всем ссылкам внутри него,
-#в реалтайме выдавая в консоль ссылку на посещённую страницу.
-#Список ссылок на внешние ресурсы выдавать в самом конце.
-#Убедиться, что одну и ту же страницу не посещаешь дважды.
+             crawl_pages_at_url(page)
+             print(page)
+    print("Мы проверили ваш сайт: ", site)
